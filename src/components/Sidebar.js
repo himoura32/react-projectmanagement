882d711e64985adeaa769e58c09e4e543922ba31
himoura32 <shawn.ahearn@outlook.com>
2022-08-18 10:02:05 -0400

    coded conditional sidebar and avatar profile pics

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,16 +2,20 @@ import { NavLink } from 'react-router-dom';
 import './Sidebar.css';
 import DashboardIcon from '../assets/dashboard_icon.svg';
 import AddIcon from '../assets/add_icon.svg';
+import Avatar from './Avatar';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 import React from 'react';
 
 export default function Sidebar() {
+  const { user } = useAuthContext();
+
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
         <div className='user'>
-          {/* avatar and username here later */}
-          <p>Hey user</p>
+          <Avatar src={user.photoURL}/>
+          <p>Hey {user.displayName}</p>
         </div>
         <nav className='links'>
           <ul>
