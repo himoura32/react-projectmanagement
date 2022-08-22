9253169d2c9bc4b77955a7d2d2eb972c4cb678f2
himoura32 <shawn.ahearn@outlook.com>
2022-08-22 10:39:52 -0400

    coded create form, react-select, assign users

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Signup from './pages/signup/Signup';
 import Project from './pages/project/Project';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import OnlineUsers from './components/OnlineUsers';
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -44,6 +45,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
